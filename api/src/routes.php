16a7818a491471c67db6cd09bc92441f752de8a8
Andrew Hedges <andrew@hedges.name>
2016-08-12 09:50:35 -0700

    Restore /vendor/

--- api/src/routes.php
+++ api/src/routes.php
@@ -2,25 +2,27 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+    // echo '<pre>';var_dump($args);exit;
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-/*
-url(r'thing/(?P<barcode_number>[0-9]+)', views.barcode ),
-url(r'things/(?P<user_id>\w+)', views.things_list),
-url(r'purchase', views.purchase)
-*/
+$app->get('/api/thing/{barcode}', function ($request, $response, $args) {
+    // echo '<pre>';var_dump($args);exit;
 
-$app->get('/api/thing/[{barcode}]', function ($request, $response, $args) {
     // return info about a thing
+    $thing_id = (int)$args['barcode'];
 });
 
-$app->get('/api/things/[{user_id}]', function ($request, $response, $args) {
+$app->get('/api/things/{user_id}', function ($request, $response, $args) {
+    // echo '<pre>';var_dump($args);exit;
+
     // return a list of the user's things
 });
 
-$app->post('', function () {});
+$app->post('/api/purchase', function ($request, $response, $args) {
+    // add a new purchase record
+});
+
+// $app->post('/api/', function ($request, $response, $args) {});
