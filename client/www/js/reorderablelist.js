9c1303fae3cd9e498563b1ff521c3d2001d4a62d
Andrew Hedges <andrew@hedges.name>
2016-08-13 18:39:38 -0700

    Add 'ago' to last purchased date in the UI

--- client/www/js/reorderablelist.js
+++ client/www/js/reorderablelist.js
@@ -98,7 +98,7 @@ var ReorderableList = (function (window, document, $, undefined) {
                         </div>
                         <span class="status ${item.status}"></span>
                         <span class="header">${item.name}</span>
-                        <div class="description">Last purchased ${item.last_purchased}</div>
+                        <div class="description">Last purchased ${item.last_purchased} ago</div>
                     </div>
                 </div>
             `;
