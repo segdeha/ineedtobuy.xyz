b756b4a949f2470b2e96e3725648391d0a1b5c50
Andrew Hedges <andrew@hedges.name>
2018-08-04 16:37:25 -0700

    Add share link

--- ineedtobuy-xyz/src/components/Header.js
+++ ineedtobuy-xyz/src/components/Header.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import localForage from 'localforage';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            token: ''
+        };
+    }
+
+    componentDidMount() {
+        localForage.getItem('intb-token')
+            .then(token => {
+                if (token) {
+                    this.setState({ token });
+                }
+            })
+            .catch(console.log);
+    }
+
     render() {
+        let { token } = this.state;
+        let className = token ? 'show' : 'hide';
+        let smsMsg = encodeURI(`Share my shopping list on iNeedToBuy.xyz! https://app.ineedtobuy.xyz/?token=${token}`);
         return (
             <header>
-                <Link to="/">
-                    <img src="/img/header.png" alt="Home" />
-                </Link>
+                <Link className="header-link" to="/" />
+                <a className={`share-link ${className}`} href={`sms:;?&body=${smsMsg}`} />
             </header>
         );
     }
